e4e0afdc779665b7a0aec33d1a203133a9d73742
colila-git <colila@gbox.adnu.edu.ph>
2026-07-14 22:44:46 +0800

    Beta Deployment

--- back_end/middleware/errorHandler.js
+++ back_end/middleware/errorHandler.js
@@ -1,6 +1,10 @@
 exports.errorHandler = (err, req, res, next) => {
 
+    console.error("========== BACKEND ERROR ==========");
     console.error(err);
+    console.error("Message:", err.message);
+    console.error("Stack:", err.stack);
+    console.error("===================================");
 
     if (res.headersSent) {
         return next(err);
@@ -8,7 +12,7 @@ exports.errorHandler = (err, req, res, next) => {
 
     return res.status(500).json({
         success: false,
-        message: "Internal server error."
+        message: err.message
     });
 
 };
